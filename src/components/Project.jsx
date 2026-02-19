ea151f041956bd0a30213926c126b186a6e9d864
IrhamIzza <Irhamizza21@gmail.com>
2026-02-19 10:24:01 +0700

    fix error

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Autoplay, Pagination} from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
 import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.png";
 import project3 from "../assets/project3.png";
@@ -19,6 +19,7 @@ export default function Project() {
       description:
         "Website desain interior modern yang menampilkan portofolio proyek, layanan, dan konsep ruang dengan tampilan elegan.",
       link: "https://web-desain-interior.vercel.app/",
+      framework: ["React", "Tailwind CSS", "Shadcn ui", "Swiper"],
     },
     {
       src: project2,
@@ -26,6 +27,7 @@ export default function Project() {
       description:
         "Website menampilkan hasil pertandingan olahraga secara langsung (real-time). Berisi skor terkini, waktu pertandingan, atau klasemen.",
       link: "https://live-score-zeta.vercel.app",
+      framework: ["React", "Tailwind CSS", "Shadcn ui", "API"],
     },
     {
       src: project3,
@@ -33,6 +35,7 @@ export default function Project() {
       description:
         "Website Anime menyediakan informasi lengkap mengenai anime dan manga, seperti sinopsis, studio, genre, jumlah episode, serta rating dari anime tersebut.",
       link: "https://anime-website-umber.vercel.app/",
+      framework: ["React", "Tailwind CSS", "Shadcn ui", "API"],
     },
     {
       src: project4,
@@ -40,6 +43,7 @@ export default function Project() {
       description:
         "Aplikasi manajemen keuangan iuran dan pengeluaran RT di perumahan. Fungsinya pengelolaan penghuni, rumah, pembayaran, serta laporan keuangan dalam bentuk grafik.",
       link: "https://github.com/IrhamIzza/Sistem_Informasi_Perumahan_FrontEnd",
+      framework: ["React", "Tailwind CSS", "Laravel", "Rest API"],
     },
     {
       src: project5,
@@ -47,6 +51,7 @@ export default function Project() {
       description:
         "Website perusahaan untuk memperkenalkan kepada masyarakat umum tentang produk pupuk pertanian dalam mengatasi kelangkaan pupuk subsidi.",
       link: "https://github.com/IrhamIzza/Jaya_Berseri_FrontEnd",
+      framework: ["Vue JS", "Tailwind CSS", "Laravel", "Rest API"],
     },
     {
       src: project6,
@@ -54,6 +59,7 @@ export default function Project() {
       description:
         "Website pencarian movie ini memudahkan menemukan informasifilm dari judul. Dilengkapi dengan detail film",
       link: "https://irhamizza.github.io/Website_Movie/",
+      framework: ["JQuery", "Bootstrap", "API"],
     },
     {
       src: project7,
@@ -61,13 +67,17 @@ export default function Project() {
       description:
         "Website informasi tata cara menanam bermacam-macam tanaman hias dalam bentuk artikel yang dapat dijadikan referensi bagi pengguna untuk merawat dan budidaya.",
       link: "https://github.com/IrhamIzza/Website_Granesia",
+      framework: ["Laravel", "Bootstrap", "Rest API"],
     },
   ];
 
   return (
     <div className="px-30 py-10">
       <div className="flex flex-col ">
-        <h1 className="text-3xl font-bold text-center"> <span className="text-primary">My</span> Project</h1>
+        <h1 className="text-3xl font-bold text-center">
+          {" "}
+          <span className="text-primary">My</span> Project
+        </h1>
         <div>
           <Swiper
             spaceBetween={30}
@@ -82,7 +92,7 @@ export default function Project() {
             {project.map((item, index) => (
               <SwiperSlide key={index}>
                 <div className="pt-5 pb-10 ">
-                  <div className="group relative overflow-hidden flex flex-col gap-2 bg-white rounded-lg  p-3 shadow-[0_0_10px_rgba(0,0,0,0.8)]">
+                  <div className="group relative overflow-hidden flex flex-col gap-2 bg-primary/30 rounded-lg  p-3 shadow-[0_0_10px_rgba(0,0,0,0.8)]">
                     <div className="">
                       <img
                         src={item.src}
@@ -91,13 +101,20 @@ export default function Project() {
                       />
                     </div>
                     <div className="flex justify-between items-center ">
-                      <div className="h-30 flex flex-col gap-2">
-                        <h1 className="text-2xl font-bold text-black">
+                      <div className="h-40 flex flex-col gap-2">
+                        <h1 className="text-2xl font-bold text-white">
                           {item.title}
                         </h1>
-                        <p className="text-gray-800 leading-tight">
+                        <p className="text-gray-300 leading-tight">
                           {item.description}
                         </p>
+                        <div className="flex gap-2 text-gray-300 text-sm font-medium">
+                          {item.framework.map((item, index) => (
+                            <div key={index} className=" p-1 rounded-sm bg-cyan-600/60">
+                              {item}
+                            </div>
+                          ))}
+                        </div>
                       </div>
                     </div>
                     <div
@@ -110,7 +127,7 @@ export default function Project() {
                         flex items-end justify-center
                     "
                     >
-                      <a href={item.link}  target="_blank">
+                      <a href={item.link} target="_blank">
                         <i className="text-6xl ph ph-arrow-square-in absolute transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 rounded-full cursor-pointer hover:text-primary transition-all ease-in"></i>
                       </a>
                     </div>
